7e627891200ade214803a56224e80c745da87c10
Andre Soares Gomes <andrsoares953@yahoo.com>
2024-01-27 01:38:01 -0300

    Update DebbugerDisplay.cs

--- C_S_HELPERS/DebbugerDisplay.cs
+++ C_S_HELPERS/DebbugerDisplay.cs
@@ -10,6 +10,34 @@ public class Person
     public string FormattedDateOfBirth => DateOfBirth.ToString("d");
 }
 
+public class BookDB
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public List<string> Tags { get; set; }
+}
+
+[DebuggerTypeProxy(typeof(BookTwoView))]
+public class BookTwoDb
+{
+    private const string Teste = "should not appear in debug";
+    public string Title { get; set; }
+    public string Author { get; set; }
+    [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+    public List<string> Tags { get; set; }
+    internal class BookTwoView
+    {
+        public string proxy = "should appear in debug.";
+        private BookTwoDb bookTwoDb;
+        public BookTwoView(BookTwoDb book)
+        {
+            this.bookTwoDb = book;
+        }
+    }
+}
+
 class Program
 {
     static void Main()
@@ -21,6 +49,40 @@ class Program
             DateOfBirth = new DateTime(1980, 1, 1)
         };
 
+        var books = new List<BookDB>
+        {
+            new BookDB
+            {
+                Title = "Professional C# 7",
+                Author = "Christian Nagel",
+                Tags = new List<string> { "Programming", "C#" }
+            },
+            new BookDB
+            {
+                Title = "Professional C# 6",
+                Author = "Christian Nagel",
+                Tags = new List<string> { "Programming", "C#" }
+            }
+        };
+
+        var booksTwo = new List<BookTwoDb>
+        {
+            new BookTwoDb
+            {
+                Title = "Professional C# 7",
+                Author = "Christian Nagel",
+                Tags = new List<string> { "Programming", "C#" }
+            },
+            new BookTwoDb
+            {
+                Title = "Professional C# 6",
+                Author = "Christian Nagel",
+                Tags = new List<string> { "Programming", "C#" }
+            }
+        };
+
         Console.WriteLine(person);
+        Console.WriteLine(books);
+        Console.WriteLine(booksTwo);
     }
 }
