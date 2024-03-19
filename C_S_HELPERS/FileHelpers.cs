c700107c1aca42f4ab5a91bd23bc7135b22d9e83
Andre Soares Gomes <andrsoares953@yahoo.com>
2024-03-18 23:25:56 -0300

    [Feat] - Added directory and file helpers

---
+++ C_S_HELPERS/FileHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace MoreDotNet.Helpers
+{
+    using System;
+    using System.IO;
+
+    public static class FileHelpers
+    {
+        public static string SaveStringToTempFile(string stringToWrite)
+        {
+            var tempFilePath = Path.GetTempFileName();
+            File.WriteAllText(tempFilePath, stringToWrite);
+            return tempFilePath;
+        }
+
+        public static string SaveByteArrayToTempFile(byte[] dataToWrite)
+        {
+            if (dataToWrite == null)
+            {
+                throw new ArgumentNullException(nameof(dataToWrite));
+            }
+
+            var tempFilePath = Path.GetTempFileName();
+            File.WriteAllBytes(tempFilePath, dataToWrite);
+            return tempFilePath;
+        }
+    }
+}
